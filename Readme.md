3bc64d388c2ad0a1d2dd641cbc893ce074ea22fd
Yuhan Zhao <aidenzhao@hotmail.com>
2023-08-02 17:27:01 -0400

    Add readme file

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Cocktale webapp
+
+1. Clone the project to local repo
+2. navigate to local repo using command tool and npm install
+3. nodemon index.js
--- index.js
+++ index.js
@@ -45,40 +45,39 @@ app.get("/", async (req, res) => {
 });
 
 app.get(["/Alcoholic", "/Non_Alcoholic"], async (req, res) => {
+	var drinkList;
+	var endpoint;
+	try {
+		endpoint = req.originalUrl.substring(1);
+		drinkList = await axios.get(
+			`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${endpoint}`
+		);
+	} catch (error) {
+		console.log(error);
+	}
 
-    var drinkList;
-    var endpoint;
-    try {
-        endpoint = req.originalUrl.substring(1);
-        drinkList = await axios.get(
-            `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${endpoint}`
-        )
-    } catch(error) {
-        console.log(error);
-    }
-
-    res.render("list.ejs", {
-        title: _.replace(_.capitalize(endpoint), "_", " "),
-        content: _.sampleSize(drinkList.data.drinks, 16)
-    })
-})
+	res.render("list.ejs", {
+		title: _.replace(_.capitalize(endpoint), "_", " "),
+		content: _.sampleSize(drinkList.data.drinks, 16),
+	});
+});
 
 app.get("/drink/:id", async (req, res) => {
-    const drinkId = req.params["id"];
-    try {
-        drink = await axios.get(
-            `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`
-        )
-        res.redirect("/")
-    } catch(error) {
-        console.log(error.message);
-    }
-})
+	const drinkId = req.params["id"];
+	try {
+		drink = await axios.get(
+			`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`
+		);
+		res.redirect("/");
+	} catch (error) {
+		console.log(error.message);
+	}
+});
 
 app.post("/search", async (req, res) => {
 	const sInput = req.body.sInput;
 	try {
-        // throw new Error("Error");
+		// throw new Error("Error");
 		drink = await axios.get(
 			`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${sInput}`
 		);
